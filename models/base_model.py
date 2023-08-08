ff79db3508acb14d20c1471efcdc297f3d97e351
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-08-08 16:35:33 -0700

    adds the test case

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,24 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
+
+    def __init__(self, *args, **kwargs):
+           """initialize an instance"""
+  
+           if kwargs:
+              for key, value in kwargs.items():
+ 
+                  if key == 'created_at' or key == 'updated_at':
+                      value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                  setattr(self, key, value)
+                  continue
+ 
+                  if key != "__class__":
+                      setattr(self, attr, value)
+          else:
+              self.id = str(uuid4())
+              self.created_at = datetime.now()
+
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
