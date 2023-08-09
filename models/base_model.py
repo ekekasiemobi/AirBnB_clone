c28b028bbc87ec8d6416f2c45f832f8390cfa1d7
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-08-08 17:08:49 -0700

    the base model has been modified for task 4

--- models/base_model.py
+++ models/base_model.py
@@ -4,26 +4,17 @@ from datetime import datetime
 class BaseModel:
 
     def __init__(self, *args, **kwargs):
-           """initialize an instance"""
-  
-        if kwargs:
-            for key, value in kwargs.items():
- 
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, value)
-                    continue
- 
-                if key != "__class__":
-                    setattr(self, attr, value)
-        else:
-              self.id = str(uuid4())
-              self.created_at = datetime.now()
-
-    def __init__(self):
+        """initialize an instance"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -37,3 +28,4 @@ class BaseModel:
         data['created_at'] = self.created_at.isoformat()
         data['updated_at'] = self.updated_at.isoformat()
         return data
+
