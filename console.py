58e808b865056b105ce95e57f04daaf56ac51927
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-08-09 04:06:29 -0700

    creates a new instance of basemodel

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import cmd
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
@@ -20,6 +21,23 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def create_instance(self, argument):
+        """Instantiate a new object of BaseModel and store it in the JSON file"""
+        arguments = argument.split()
+        if not arguments:
+            print("** Missing class name **")
+            return
+
+        class_name = arguments[0]
+        if class_name not in self.classes:
+            print("** Class does not exist **")
+            return
+
+        new_object = self.classes[class_name]()
+        new_object.save()
+        print(new_object.id)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
