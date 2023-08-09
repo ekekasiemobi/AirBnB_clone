adc8d7e96fec1ec75d10748032296865bf90258b
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-08-08 18:01:56 -0700

    the console displays a prompt and execute some commands

--- console.py
+++ console.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import cmd
 
 class HBNBCommand(cmd.Cmd):
@@ -9,8 +11,15 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """Exit the program"""
-        print()  # Print a newline before exiting
+        print()
         return True
+
+    def help_quit(self):
+        print("Quit command to exit the program")
+        print()
+
+    def emptyline(self):
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
