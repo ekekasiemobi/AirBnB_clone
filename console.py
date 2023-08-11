3a2dc51d48da28a578af210b0e4f62c2bf964ceb
RealKingTino <tmowizzy@gmail.com>
2023-08-11 11:38:32 -0700

    import classes

--- console.py
+++ console.py
@@ -6,6 +6,12 @@ import sys
 import models
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
