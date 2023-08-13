9e8205bcd9ae4def960f80b9a6684eb7f315ebe2
RealKingTino <tmowizzy@gmail.com>
2023-08-13 02:35:35 -0700

    fix reload issue in file storage

--- models/base_model.py
+++ models/base_model.py
@@ -11,22 +11,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """initialize an instance"""
+        dateformat = '%Y-%m-%dT%H:%M:%S.%f'
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        models.storage.new(self)
 
-        if kwargs:
+        if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
-                    setattr(self, key, value)
-
-    def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clsname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clsname, self.id, self.__dict__)
+                    self.__dict__[key] = datetime.strptime(value, dateformat)
+                else:
+                    self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
@@ -38,7 +35,12 @@ class BaseModel:
         Return the dictionary of the BaseModel instance.
         """
         data = self.__dict__.copy()
-        data['__class__'] = self.__class__.__name__
         data['created_at'] = self.created_at.isoformat()
         data['updated_at'] = self.updated_at.isoformat()
+        data['__class__'] = self.__class__.__name__
         return data
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clsname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsname, self.id, self.__dict__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,48 +1,40 @@
 #!/usr/bin/python3
 
 import json
+import os
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import sys
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        data = {}
-        for key, obj in self.__objects.items():
-            data[key] = obj.to_dict()
-        with open(self.__file_path, 'w') as f:
-            json.dump(data, f)
-
+        """Serialize __objects to the JSON file __file_path."""
+        objdict = {}
+        for key in FileStorage.__objects:
+            objdict[key] = FileStorage.__objects[key].to_dict()
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
     def reload(self):
-        try:
-            with open(FileStorage.__file_path, 'r') as f:
-                data = json.load(f)
-                for key, value in data.items():
-                    cls_name, obj_id = key.split('.')
-                    cls = eval(cls_name)
-                    if cls_name == "User":
-                        obj = User(**value)
-                    elif cls_name == "Place":
-                        obj = Place(**value)
-                    elif cls_name == "State":
-                        obj = State(**value)
-                    elif cls_name == "City":
-                        obj = City(**value)
-                    elif cls_name == "Amenity":
-                        obj = Amenity(**value)
-                    elif cls_name == "Review":
-                        obj = Review(**value)
-                    else:
-                        continue
-                    self.new(obj)
-        except FileNotFoundError:
-            pass
+        """deserializes the JSON file to __objects"""
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                my_dict = json.load(f)
+                for key, value in my_dict.items():
+                    name = sys.modules[__name__]
+                    my_class = getattr(name, value['__class__'])
+                    self.__objects[key] = my_class(**value)
