65d77ad93fff927f83d385662bba6382ffc10254
RealKingTino <tmowizzy@gmail.com>
2023-08-13 10:48:12 -0700

    pycode style

--- models/review.py
+++ models/review.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """ Represent the review class
-    
+
     Attributes:
         place_id (str): The Place id.
         user_id (str): The User id.
